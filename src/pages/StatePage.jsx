50d3ba5658a18ce047032ff49b614994d74fcd9c
Sayyed Irfan <sayyedirfan255@gmail.com>
2023-11-11 17:23:01 +0530

    state & conditional rendering

--- src/pages/StatePage.jsx
+++ src/pages/StatePage.jsx
@@ -1,14 +1,47 @@
 import React, { useState } from "react";
 
 const StatePage = () => {
+  // const [state, setState] = useState(0);
 
-const [state, setState]=useState("Irfan")
+  // const changeState = () => {
+  //   setState(state + 1);
+  // };
 
-console.log(state)
+  const [isShow, setIsShow] = useState(false);
+
+  const handleShow = () => {
+    setIsShow(!isShow);
+  };
 
   return (
     <div className="container p-4">
-      <h1>{state}</h1>
+      {isShow ? (
+        <>
+          <h1>Heading 111</h1>
+          <h1>Heading 222</h1>
+        </>
+      ) : (
+        <p>
+          kjnckjnjckjdncjnjcdsjk lknskcndscndksnckjdsn kjnkjcndkjnckjdsn
+          kjncjksnskj kjncnsjk
+        </p>
+      )}
+
+      <button
+        className={isShow ? "btn btn-success me-4" : "btn btn-danger me-4"}
+        onClick={handleShow}
+      >
+        {isShow ? "Hide" : "Show"}
+      </button>
+
+      {/* <h1>{state}</h1>
+
+      <button className="btn btn-danger me-4" onClick={changeState}>
+        Increment
+      </button>
+      <button className="btn btn-danger" onClick={() => setState(state - 1)}>
+        Decrement
+      </button> */}
     </div>
   );
 };
